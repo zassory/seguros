6c52020ad82bedb1e3b0dab655264f9916c04ce7
Nicolas <nicolas.programador@gmail.com>
2022-12-12 14:27:08 -0300

    Cotizacion lista, falta memorizar los resultados para evitar el re render

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -6,8 +6,9 @@ export const Resultado = () => {
   const { resultado , datos } = useCotizador();
   const { marca , plan , year } = datos;
 
-  const [nombreMarca] = MARCAS.filter(( m ) => m.id === Number(marca));
-  console.log(nombreMarca);
+  const [nombreMarca] = MARCAS.filter( m  => m.id === Number(marca));
+  const [nombrePlan] = PLANES.filter( p => p.id === Number(plan));
+  
 
   if(resultado === 0) return null;
 
@@ -21,6 +22,19 @@ export const Resultado = () => {
         <span className="font-bold">Marca: </span>
         {nombreMarca.nombre}
       </p>
+      <p className="my-2">
+        <span className="font-bold">Plan: </span>
+        {nombrePlan.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Año del Auto: </span>
+        {year}
+      </p>
+      <p className="my-2 text-2xl">
+        <span className="font-bold">Total Cotización: </span>
+        {resultado}
+      </p>
+
     </div>
   )
 }
